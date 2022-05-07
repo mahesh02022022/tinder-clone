a634a0b11d256bf732eb67b5793fc664a004cbf1
mahemall <free.ibm@gmail.com>
2022-05-08 02:16:17 +0530

    Initial commit

---
+++ precache-manifest.f2bc8ce79fb89b3d1295f2e6d4287fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52b764a93487f9a61e9d1dc31575723",
+    "url": "/tinder-clone/index.html"
+  },
+  {
+    "revision": "e87cbdf6af0fff3c6f9a",
+    "url": "/tinder-clone/static/css/main.b34aa130.chunk.css"
+  },
+  {
+    "revision": "6a0536251198f26b6765",
+    "url": "/tinder-clone/static/js/2.510261c1.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/tinder-clone/static/js/2.510261c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e87cbdf6af0fff3c6f9a",
+    "url": "/tinder-clone/static/js/main.bebef40d.chunk.js"
+  },
+  {
+    "revision": "de86c0115f6746d23e21",
+    "url": "/tinder-clone/static/js/runtime-main.acd27e62.js"
+  }
+]);
